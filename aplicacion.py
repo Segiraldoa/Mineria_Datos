8969656f5845e6ab25e754f2555b9f57af65f833
Segiraldoa <93294390+Segiraldoa@users.noreply.github.com>
2025-01-31 09:39:16 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -4,5 +4,7 @@ def main():
   st.title("Clasificación de la base de datos mnist")
   st.markdown("Suba la imagen para clasificar")
 
+  uploaded_file = st.file_uploader("Selecciona una imagen",type=["jpg","png","jpeg"])
+
 if __name__=='__main__':
   main()
